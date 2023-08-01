56399636e912025908549bf39fba54d0654b39e3
mdmanik74 <manikmhamud74@outlook.com>
2023-08-01 18:20:36 +0600

    onboard desgin and text

--- lib/on_boarding/on_boarding_page.dart
+++ lib/on_boarding/on_boarding_page.dart
@@ -25,7 +25,37 @@ class _OnboardViewState extends State<OnboardView> {
               height: media.height,
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.start,
-                children: [Image.asset("assets/img/on_1.png",width: media.width,fit: BoxFit.fitWidth,)],
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Image.asset(
+                    "assets/img/on_1.png",
+                    width: media.width,
+                    fit: BoxFit.fitWidth,
+                  ),
+                  SizedBox(
+                    height: media.width * 0.1,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 15),
+                    child: Text(
+                      'Track Your Goal',
+                      style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.w700,
+                          color: TColor.black),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 15),
+                    child: Text(
+                      "Don't worry if you have trouble determining your goals, We can help you determine your goals and track your goals",
+                      style: TextStyle(
+                          fontSize: 14,
+
+                          color: TColor.gray),
+                    ),
+                  ),
+                ],
               ),
             );
           })
